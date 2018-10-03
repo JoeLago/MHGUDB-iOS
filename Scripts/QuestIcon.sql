8ae7bc212c56f4c862df8d3e1082728f29f08df7
Joe Lagomarsino <joe.lagomarsino@gmail.com>
2018-10-02 22:21:36 -0700

    Database update

--- Scripts/QuestIcon.sql
+++ Scripts/QuestIcon.sql
@@ -1,6 +1,6 @@
 ALTER TABLE quests ADD COLUMN icon_name;
 
-UPDATE quests SET icon_name = 'Quest-Icon-White.png';
+UPDATE quests SET icon_name = 'icon_quest.png';
 UPDATE quests SET icon_name = (SELECT items.icon_name FROM items WHERE quests.goal LIKE '%'||items.name||'%')
 WHERE quests.icon_name IS NULL OR EXISTS (SELECT items.icon_name FROM items WHERE quests.goal LIKE '%'||items.name||'%');
 UPDATE quests SET icon_name = (SELECT monsters.icon_name FROM monsters WHERE quests.goal LIKE '%'||monsters.name||'%')
